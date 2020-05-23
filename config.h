d470c1b468c8d7ce210791ddc7b9b679fb9b9615
Hamza Mughal <hamzashafique@outlook.com>
2020-05-23 16:27:53 +0500

    changed primary color

--- config.h
+++ config.h
@@ -13,9 +13,9 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#FFC300" },
+	[SchemeSelHighlight] = { "#FFC300", "#C70039" },
+	[SchemeNormHighlight] = { "#900C3E", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
