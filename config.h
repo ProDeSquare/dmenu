749f08d70e280104beca54c7ec3acac6a7c6f14d
Hamza Mughal <hamza@prodesquare.com>
2021-01-04 10:20:54 +0500

    removed the default center option

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 760;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -20,7 +20,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 10;
+static unsigned int lines      = 0;
 static unsigned int lineheight = 30;         /* -h option; minimum height of a menu line     */
 
 /*
