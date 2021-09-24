15163834111f81bdb47c6f42d8bf260946c7cbf7
Hamza Mughal <hamza@prodesquare.com>
2021-09-25 04:01:48 +0500

    dmenu grid

--- config.h
+++ config.h
@@ -19,9 +19,10 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
 static unsigned int lineheight = 30;         /* -h option; minimum height of a menu line     */
+static unsigned int columns    = 0;
 
 /*
  * Characters not considered part of a word while deleting words
