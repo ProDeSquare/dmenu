7b0b8f566ff6c3e911d0669e1ff4d06748e91432
Hamza Mughal <hamza@prodesquare.com>
2021-09-25 03:57:01 +0500

    removed prompt, moved menu to the top

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 760;                    /* minimum width when centered */
@@ -9,7 +9,7 @@ static int min_width = 760;                    /* minimum width when centered */
 static const char *fonts[] = {
 	"Jetbrains Mono:size=11"
 };
-static const char *prompt      = "Run ";      /* -p  option; prompt to the left of input field */
+static const char *prompt      = "";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#E6C176", "#282C34" },
