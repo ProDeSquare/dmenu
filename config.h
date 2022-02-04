e46ec6ef7b68716462ff3eb08b4740bff1badf64
Hamza Mughal <hamza@prodesquare.com>
2022-02-05 01:18:57 +0500

    colorscheme updation

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 760;                    /* minimum width when centered */
+static int min_width = 400;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Jetbrains Mono:size=10"
@@ -12,10 +12,10 @@ static const char *fonts[] = {
 static const char *prompt      = "";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#E6C176", "#282C34" },
-	[SchemeSel] = { "#282C34", "#E6C176" },
+	[SchemeNorm] = { "#E6C176", "#1c2023" },
+	[SchemeSel] = { "#1c2023", "#E6C176" },
 	[SchemeSelHighlight] = { "#C84B4A", "#E6C176" },
-	[SchemeNormHighlight] = { "#B771CD", "#282C34" },
+	[SchemeNormHighlight] = { "#B771CD", "#1c2023" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
